644e301094fe09b2c9f9193d7372be7d270ab1f3
JonusJoseph <jonusjoseph@gmail.com>
2017-01-20 19:18:22 +0530

    fping multiple IP's
    fping multiple IP's

---
+++ jjfping.sh
@@ -0,0 +1,9 @@
+#Written by Jonus Joseph
+#!/bin/bash
+for x in `cat ip.txt`
+do
+#echo "##########################################################"
+#ping -c 3 $x
+fping $x #| awk NR==1
+#echo "##########################################################"
+done
