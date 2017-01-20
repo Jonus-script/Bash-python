e68a7c92537198ad282cae641ac9b3a4d7912876
JonusJoseph <jonusjoseph@gmail.com>
2017-01-20 19:20:27 +0530

    traceroute for mutiple servers
    trace route for multiple servers

---
+++ jjtracerout.sh
@@ -0,0 +1,8 @@
+#Written by Jonus Joseph
+#!/bin/bash
+for x in `cat  /tmp/devices_list.txt`
+do
+#echo"==================================================="
+/bin/traceroute $x >> /tmp/result_traceroute.txt  2>&1
+#echo"==================================================="
+done
